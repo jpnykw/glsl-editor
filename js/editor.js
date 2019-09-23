2464adbbdf60ae9d3f8edc67f14360ff48f320ad
JPNYKW <haruto_17n1101083@nnn.ed.jp>
2019-09-23 19:46:53 +0900

    fix Style, add AutoComplete

--- js/editor.js
+++ js/editor.js
@@ -3,9 +3,17 @@
 
     window.onload = () => {
         const editor = ace.edit('editor');
+        editor.$blockScrolling = Infinity;
+
         editor.setFontSize(13);
         editor.getSession().setMode("ace/mode/glsl");
         editor.setTheme('ace/theme/tomorrow_night_eighties');
+
+        editor.setOptions({
+            enableBasicAutocompletion: true,
+            enableSnippets: true,
+            enableLiveAutocompletion: true
+        });
 
         // editor.setValue('// write your cool shader in here.');
         editor.setValue('precision mediump float;\nuniform vec2 resolution;\n\nvoid main() {\n\t// write your cool shader in here ...\n\tvec2 position = (gl_FragCoord.xy * 2.0 - resolution) / min(resolution.x, resolution.y);\n\tvec3 color = vec3(position.x, position.y, 0.0);\n\tgl_FragColor = vec4(color, 1.0);\n}');
